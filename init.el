a6678ab8a17ebb8f85d7fcf84854b8ea16df9e56
Angelo Montenegro <amontene823@gmail.com>
2024-12-12 17:25:13 -0500

    set-face-attribute to align org-column and org-column-title

--- init.el
+++ init.el
@@ -477,7 +477,8 @@
     (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
     (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
     (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-column nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-column nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-column-title nil :inherit 'fixed-pitch)
     (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
   (defun am/org-setup()
